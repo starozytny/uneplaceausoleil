f81c355e77e4172d4ae3f783542426e9b00fedb9
Chanbora <chanbora@logilink.fr>
2020-06-18 11:00:46 +0200

    init front end

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+var Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    .copyFiles({
+        from: './assets/themes/default/front/fonts',
+        to: 'themes/default/front/fonts/[path][name].[ext]',
+    })
+    .copyFiles({
+        from: './assets/themes/default/front/images',
+        to: 'themes/default/front/images/[path][name].[ext]',
+    })
+    .copyFiles({
+        from: './assets/themes/default/back/fonts',
+        to: 'themes/default/back/fonts/[path][name].[ext]',
+    })
+    .copyFiles({
+        from: './assets/themes/default/back/images',
+        to: 'themes/default/back/images/[path][name].[ext]',
+    })
+
+    .configureFilenames({
+        css: 'css/[name].css',
+        js: 'js/[name].js'
+    })
+
+    /*
+     * ENTRY CONFIG
+     *
+     */
+    .addEntry('default_app', './assets/themes/default/front/js/default.jsx')
+
+    .addEntry('default_admin', './assets/themes/default/back/js/default.jsx')
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    /*
+     * FEATURE CONFIG
+     *
+     */
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    // enables Sass/SCSS support
+    .enableSassLoader()
+
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
+
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    //.enableIntegrityHashes(Encore.isProduction())
+
+    // uncomment if you're having problems with a jQuery plugin
+    //.autoProvidejQuery()
+
+    // uncomment if you use API Platform Admin (composer req api-admin)
+    //.enableReactPreset()
+    //.addEntry('admin', './assets/js/admin.js')
+;
+
+module.exports = Encore.getWebpackConfig();
